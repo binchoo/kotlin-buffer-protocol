e55ae4aad6cb6fc95a1c2c2eeff42fe7bd02c33e
binchoo <079111w@gmail.com>
2020-08-12 16:14:15 +0900

    Change FuntinalInterfaces to Lambda.

--- dataprotocol/src/main/java/com/corndog/dataprotocol/typehandle/TypeHandler.kt
+++ dataprotocol/src/main/java/com/corndog/dataprotocol/typehandle/TypeHandler.kt
@@ -1,14 +1,3 @@
 package dataprotocol.typehandle
 
-//interface TypeHandler<T> {
-//    fun handle(data: T, handlingHint: Int)
-//}
-
-//interface ByteHandler: TypeHandler<Byte>
-//interface CharHandler: TypeHandler<Char>
-//interface ShortHandler: TypeHandler<Short>
-//interface IntHandler: TypeHandler<Int>
-//interface FloatHandler: TypeHandler<Float>
-//interface DoubleHandler: TypeHandler<Double>
-
 typealias TypeHandler<T> = ((data: T, handlingHint: Int)-> Unit)
