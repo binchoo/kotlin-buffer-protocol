1aee5683052151427ae9aa9f9085f40eaf90049c
binchoo <079111w@gmail.com>
2020-08-13 19:10:39 +0900

    Add littel endian test for DelimProtocolBufferReaderTest.kt

--- dataprotocol/src/test/java/com/corndog/dataprotocol/DelimProtocolBufferReaderTest.kt
+++ dataprotocol/src/test/java/com/corndog/dataprotocol/DelimProtocolBufferReaderTest.kt
@@ -1,9 +1,11 @@
 package com.corndog.dataprotocol
 
 import com.corndog.dataprotocol.extension.DelimProtocolBufferReader
+import dataprotocol.buffered.ProtocolReader
 import org.junit.Assert.*
 import org.junit.Test
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class DelimProtocolBufferReaderTest {
 
@@ -11,24 +13,49 @@ class DelimProtocolBufferReaderTest {
     val string = strings.joinToString("\n")
     val stringResult = ArrayList<String>()
 
-    val buffer = ByteBuffer.allocate(2 * string.length).also {buffer->
-        string.toCharArray().forEach {character->
-            buffer.putChar(character)
-        }
-    }
+    val bufferBig = stringToByteBuffer(ByteOrder.BIG_ENDIAN)
+    val bufferLittle = stringToByteBuffer(ByteOrder.LITTLE_ENDIAN)
 
-    val reader = DelimProtocolBufferReader("\n", buffer).also {
-        it.addStringChunkHandler { data, handlingHint ->
-            println(data)
-            stringResult.add(data)
+    lateinit var reader: ProtocolReader
+
+    @Test
+    fun delimProtocolReader_bigCorrect() {
+        reader = readerForOrder(ByteOrder.BIG_ENDIAN, bufferBig)
+        reader.readByData()
+
+        stringResult.forEachIndexed {index, str->
+            assertEquals(strings[index], str)
         }
+
+        stringResult.clear()
     }
 
     @Test
-    fun delimProtocolReader_printCorrect() {
+    fun delimProtocolReader_littleCorrect() {
+        reader = readerForOrder(ByteOrder.LITTLE_ENDIAN, bufferLittle)
         reader.readByData()
+
         stringResult.forEachIndexed {index, str->
             assertEquals(strings[index], str)
+        }
+
+        stringResult.clear()
+    }
+
+    private fun stringToByteBuffer(order: ByteOrder): ByteBuffer {
+        return ByteBuffer.allocate(2 * string.length).also { buffer->
+            string.toCharArray().forEach {character->
+                buffer.order(order).putChar(character)
+            }
+        }
+    }
+
+    private fun readerForOrder(order: ByteOrder, buffer: ByteBuffer): ProtocolReader {
+        return DelimProtocolBufferReader("\n", buffer, order).also {
+            it.addStringChunkHandler { data, handlingHint ->
+                println(data)
+                stringResult.add(data)
+            }
         }
     }
 }
