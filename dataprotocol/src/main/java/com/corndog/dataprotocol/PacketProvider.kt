a84e43bf8905f3483abb1e569afc5f1b6a1e3470
binchoo <079111w@gmail.com>
2020-09-30 03:27:13 +0900

    Add READ_ALL constant to PacketProvider

--- dataprotocol/src/main/java/com/corndog/dataprotocol/PacketProvider.kt
+++ dataprotocol/src/main/java/com/corndog/dataprotocol/PacketProvider.kt
@@ -1,10 +1,13 @@
 package com.corndog.dataprotocol
 
-import java.io.InputStream
 import java.nio.ByteBuffer
 
 interface PacketProvider {
     fun provide(size: Int = -1): ByteBuffer
 
     fun available(size: Int = -1): Boolean
+
+    companion object {
+        val READ_ALL = -1
+    }
 }
